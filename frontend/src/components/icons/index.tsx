1f7749a025a0ffdcd95ef2e3b8d483fba86d2a81
DreckSallow <dikson1.aranda.cabrera@gmail.com>
2023-12-07 22:37:47 -0500

    feat(front): add cart and search icons

---
+++ frontend/src/components/icons/index.tsx
@@ -0,0 +1,19 @@
+export function CartIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24">
+      <path d="M21.822 7.431A1 1 0 0 0 21 7H7.333L6.179 4.23A1.994 1.994 0 0 0 4.333 3H2v2h2.333l4.744 11.385A1 1 0 0 0 10 17h8c.417 0 .79-.259.937-.648l3-8a1 1 0 0 0-.115-.921zM17.307 15h-6.64l-2.5-6h11.39l-2.25 6z">
+      </path>
+      <circle cx="10.5" cy="19.5" r="1.5"></circle>
+      <circle cx="17.5" cy="19.5" r="1.5"></circle>
+    </svg>
+  )
+}
+
+export function SearchIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24">
+      <path d="M10 18a7.952 7.952 0 0 0 4.897-1.688l4.396 4.396 1.414-1.414-4.396-4.396A7.952 7.952 0 0 0 18 10c0-4.411-3.589-8-8-8s-8 3.589-8 8 3.589 8 8 8zm0-14c3.309 0 6 2.691 6 6s-2.691 6-6 6-6-2.691-6-6 2.691-6 6-6z">
+      </path>
+    </svg>
+  )
+}

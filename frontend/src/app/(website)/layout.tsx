9a14685a57450e9d15d08f905e7f0b5e4c204c51
DreckSallow <dikson1.aranda.cabrera@gmail.com>
2023-12-07 22:38:32 -0500

    feat(front): improve the navbar layout

--- frontend/src/app/(website)/layout.tsx
+++ frontend/src/app/(website)/layout.tsx
@@ -1,24 +1,33 @@
+import { CartIcon, SearchIcon } from "@/components/icons";
+
 interface Children {
   children: React.ReactNode
 }
 export default function WebsiteLayout({ children }: Children) {
   return (
-    <>
-      <nav className="flex flex-row align-center justify-between">
-        <div>LOGO</div>
-        <div>
-          <input type="search" placeholder="Buscar" />
-        </div>
-        <ul className="flex flex-row align-center justify-between">
-          <li>
-            Carrito
-          </li>
-          <li>
-            SignIn
-          </li>
-        </ul>
-      </nav>
+    <main className="web-main">
+      <NavBar />
       {children}
-    </>
+    </main>
+  )
+}
+
+function NavBar() {
+  return (
+    <nav className="flex-rw justify-between px-4 bg-white gap-3">
+      <div>LOGO</div>
+      <div className="flex-rw search-out">
+        <SearchIcon />
+        <input type="search" placeholder="Buscar" className="ml-2 outline-none" />
+      </div>
+      <ul className="flex-rw justify-between gap-3">
+        <li className="cursor-pointer rounded-full p-2 border border-gray-300">
+          <CartIcon />
+        </li>
+        <li className="btn-pry">
+          Login
+        </li>
+      </ul>
+    </nav>
   )
 }
